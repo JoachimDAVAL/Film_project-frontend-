3c78fc4f5c1aea44c3a6011cc32c0baafe1f1f6b
JoachimDAVAL <joachim.daval01@gmail.com>
2025-03-26 12:26:58 +0100

    gfyi

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -54,7 +54,7 @@ export default function MovieCard({ movie }: MovieCardProps) {
       {showDetails && (
         <Link to={`/movie/${movie.id}`}>
           <div 
-          className="absolute bg-white bg-opacity-100 top-0 left-0 right-0 bottom-0 text-white p-4 rounded-xl flex flex-col justify-around items-center shadow-md lg:overflow-hidden transition-transform hover:scale-0 lg:hover:scale-105 duration-500 w-full max-w-full"
+          className="absolute bg-white bg-opacity-100 top-0 left-0 right-0 bottom-0 text-white p-4 rounded-xl flex flex-col justify-around items-center shadow-md overflow-hidden  w-full max-w-full"
           style={{backgroundImage: movie.poster_path
             ? `url(https://image.tmdb.org/t/p/w342${movie.poster_path})`
             : `url(${defaultImage})`,
